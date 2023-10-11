1fd32d242b912d4abf71b6c89e1f957e4accfc20
João Marcos Cardoso Ramos da Silva <joaomarcosxix@gmail.com>
2023-10-11 11:38:27 -0300

    initial stuff

--- src/components/JsonUploader.svelte
+++ src/components/JsonUploader.svelte
@@ -1,9 +1,36 @@
-<h1>JSON Tree Viewer</h1>
+<h1 class="json-uploader__title">JSON Tree Viewer</h1>
 
-<p>Simple JSON Viewer that runs completely on-client. No data exchange </p>
+<p class="json-uploader__subtitle">Simple JSON Viewer that runs completely on-client. No data exchange </p>
 
-<Button>Load JSON</Button>
+<button on:click={uploadFile}>
+  Load JSON
+</button>
+
+{#if error}
+  <p class="json-uploader__error">Invalid file. Please load a valid JSON file.</p>
+{/if}
 
 <script>
-	import Button from "./Button.svelte";
+  let error = false;
+  let file = null;
+
+	const uploadFile = () => {
+		console.log('uploading file')
+	}
 </script>
+
+<style>
+  .json-uploader__subtitle {
+    margin: 20px 0;
+  }
+
+  .json-uploader__title {
+    margin: 0;
+  }
+
+  .json-uploader__error {
+    /* display: none; */
+    color: red;
+  }
+</style>
+
